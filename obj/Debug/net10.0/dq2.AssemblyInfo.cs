48e4445a2eb2ce4ecdde07168bd345b25565a22a
Harshal Sharma <ghafhrjr@gmail.com>
2026-08-19 04:14:45 -0700

    adding teacher duties

--- obj/Debug/net10.0/dq2.AssemblyInfo.cs
+++ obj/Debug/net10.0/dq2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dq2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1a5d1d313c2189eff6b9b4701af4fd47800dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08968449378fdc97c6c59bceae70ba2bc467f78e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dq2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dq2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ techo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program
+{
+    class techo
+    {
+        public void menu()
+        {
+            Console.WriteLine("!!!!!WELCOME!!!!!");
+            Console.WriteLine("HERE IS YOUR DASHBOARD" );
+
+            Console.WriteLine(" 1. VIEW MY STUDENTS");
+            Console.WriteLine(" 2. MARK  STUDENTS ATTENDANCE");
+            Console.WriteLine(" 3. ENTER STUDENTS PERFORMANCE");
+            Console.WriteLine(" 4. VIEW SCHEDULE ");
+            Console.WriteLine(" 5. CHECK NOTEBOOKS ");
+            Console.WriteLine(" 6. LOGOUT");
+        }
+
+    }
+}
