028ea93a1c21b52b4ffa8cfeeb4e74cea4a40dd1
Harshal Sharma <ghafhrjr@gmail.com>
2026-08-20 00:19:38 -0700

    adding roles

---
+++ models/role.cs
@@ -0,0 +1,10 @@
+namespace program.Models;
+
+public class Role
+{
+    public int RoleId { get; set; }
+
+    public string RoleName { get; set; } = string.Empty;
+
+    public ICollection<User> Users { get; set; } = new List<User>();
+}
